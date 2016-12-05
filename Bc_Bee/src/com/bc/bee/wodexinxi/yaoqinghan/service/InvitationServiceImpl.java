a7b5d2bbed078d4fabf9e996c7ca431e3ed0759f
jiashuaiyong <1365203367@qq.com>
2016-12-05 14:26:34 +0800

    增加两个删除的文件

---
+++ Bc_Bee/src/com/bc/bee/wodexinxi/yaoqinghan/service/InvitationServiceImpl.java
@@ -0,0 +1,44 @@
+package com.bc.bee.wodexinxi.yaoqinghan.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bc.bee.entity.Invitation;
+import com.bc.bee.entity.RecInfo;
+import com.bc.bee.entity.Resume;
+import com.bc.bee.wodexinxi.yaoqinghan.dao.YaoqingbiaoRecInfoDaoImpl;
+import com.bc.bee.wodexinxi.yaoqinghan.dao.InvitationDaoImpl;
+import com.bc.bee.wodexinxi.yaoqinghan.dao.ResumeDaoImpl;
+
+@Service
+@Transactional(readOnly=true)
+public class InvitationServiceImpl {
+	@Resource
+	private InvitationDaoImpl InvitationDaoImpl;
+	@Resource
+	private YaoqingbiaoRecInfoDaoImpl RecInfoDaoImpl;
+	
+	@Resource
+	private ResumeDaoImpl resumedaoimpl;
+	
+	public List<Invitation> findByTUId(int TUId){
+		return this.InvitationDaoImpl.findByTUId(TUId);
+	}
+	public RecInfo findRecByRlId(int RlId){
+		return this.RecInfoDaoImpl.findByRlId(RlId);
+	}
+	@Transactional(readOnly=false)
+	public void deleteByDeId(int InId){
+		this.InvitationDaoImpl.deleteByInId(InId);
+	}
+	public List<Invitation> findByPUId(int PUId){
+		return this.InvitationDaoImpl.findByPUId(PUId);
+	}
+	public Resume findResumeByTUId(int TUId){
+		return this.resumedaoimpl.findByTUId(TUId);
+	}
+}
