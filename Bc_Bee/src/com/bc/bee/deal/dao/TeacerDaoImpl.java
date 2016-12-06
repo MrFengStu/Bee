7c71f3f391224674e70d953364ae9bcd6e5ed613
wangzifan <1039738015@qq.com>
2016-12-06 11:51:11 +0800

    增加交易

---
+++ Bc_Bee/src/com/bc/bee/deal/dao/TeacerDaoImpl.java
@@ -0,0 +1,20 @@
+package com.bc.bee.deal.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.bc.bee.entity.TeaCer;
+import com.framework.BaseDao;
+
+@Repository
+public class TeacerDaoImpl extends BaseDao<TeaCer, String> {
+
+	public TeaCer findByTUId(Integer TUId){
+		try {
+			return super.findOne("from TeaCer te where te.TUId=?", new Object[]{TUId});
+		} catch (Exception e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
