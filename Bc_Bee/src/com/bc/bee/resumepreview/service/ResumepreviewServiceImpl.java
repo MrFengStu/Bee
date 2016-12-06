74f4f0f27db15b840db05137d17c08a86c43b4e7
dingjiawei <327396238@qq.com>
2016-12-06 16:45:11 +0800

    增加控制器recrumentpreview.java

---
+++ Bc_Bee/src/com/bc/bee/resumepreview/service/ResumepreviewServiceImpl.java
@@ -0,0 +1,27 @@
+package com.bc.bee.resumepreview.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bc.bee.entity.RecInfo;
+import com.bc.bee.entity.Resume;
+import com.bc.bee.resumepreview.dao.ResumepreviewDaoImpl;
+
+ 
+@Service
+@Transactional(readOnly=false)
+public class ResumepreviewServiceImpl {
+	
+	@Resource
+	private ResumepreviewDaoImpl resumepreviewDaoImpl;
+	
+	
+	public java.util.List<Resume> findById(Integer TUId){
+		java.util.List<Resume> list = resumepreviewDaoImpl.findById(TUId);
+		return list;
+	}
+	
+
+}
